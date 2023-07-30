83c03d248b0b3835d36ff6f94d828c5330ad2f54
Sundram Yadav <hot2shortcut@gmail.com>
2023-07-30 16:42:35 +0530

    added more codes

---
+++ DataStructureAndMordernOperatosAndStrings/SET And Map/Map.js
@@ -0,0 +1,59 @@
+"use strict";
+const rest = new Map();
+console.log(rest);
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenzy,Itely");
+console.log(rest.set(2, "Lisban,Portugal"));
+
+rest
+  .set("Categories", ["Italian", "Pizzaria", "Vegetarian", "organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open at :D")
+  .set(false, "We are closed at :(");
+
+console.log(rest.get("name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+console.log("To check if map has:");
+console.log(rest.has("Categries"));
+console.log("======================================================");
+const numbers = [1, 4, 9];
+const roots = numbers.map((num) => Math.sqrt(num));
+console.log(roots);
+
+const square = numbers.map((x) => x ** 2);
+console.log(square);
+console.log("======================================================");
+const kvArray = [
+  { key: 1, value: 10 },
+  { key: 2, value: 20 },
+  { key: 3, value: 30 },
+];
+
+const reformattedArray = kvArray.map(({ key, value }) => ({ [key]: value }));
+
+console.log(reformattedArray); // [{ 1: 10 }, { 2: 20 }, { 3: 30 }]
+console.log(kvArray);
+console.log("======================================================");
+const arrayLike = {
+  length: 3,
+  0: 2,
+  1: 3,
+  2: 4,
+  3: 5, // ignored by map() since length is 3
+};
+console.log(Array.prototype.map.call(arrayLike, (x) => x ** 2));
+// [ 4, 9, 16 ]
+console.log("======================================================");
+const arr = [1, 2];
+rest.set(arr, "Test");
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest);
+console.log("======================================================");
+console.log(
+  [1, , 3].map((x, index) => {
+    console.log(`Visit ${index}`);
+    return x * 2;
+  })
+);
